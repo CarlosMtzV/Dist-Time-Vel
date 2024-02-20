8c943a8a5b3a190a8da2f24d94f3aa9e18acb155
CarlosMtzV <159225079+CarlosMtzV@users.noreply.github.com>
2024-02-20 00:57:43 -0600

    Calculate Distance velocity and time

---
+++ Dist,vel,Time/CalVel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dist_vel_Time
+{
+    public class CalVel
+    {
+        // Propiedades 
+        public double Distance { get; set; }
+        public double Time { get; set; }
+
+        // Constructor 
+        public CalVel(double distance, double time)
+        {
+            Distance = distance;
+            Time = time;
+        }
+
+        // Método 
+        public double CalcularVelocidad()
+        {
+            return Distance / Time;
+        }
+    }
+}
---
+++ Dist,vel,Time/Program.cs
@@ -0,0 +1,13 @@
+﻿using Dist_vel_Time;
+
+Console.Write("Enter the distance traveled (in meters): ");
+double distance = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Enter the elapsed time (in seconds): ");
+double time = Convert.ToDouble(Console.ReadLine());
+
+CalVel miVelocidad = new CalVel(distance, time);
+
+double velocidad = miVelocidad.CalcularVelocidad();
+
+Console.WriteLine("The speed is: " + velocidad + " meters / second ");
